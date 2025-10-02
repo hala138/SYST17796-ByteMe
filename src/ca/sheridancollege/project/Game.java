655317d87b0ffc59683eb2b586183b9231edff39
hala138 <issaayoh@sheridancollege.ca>
2025-10-02 16:12:38 -0400

    First commit - project starter

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -48,7 +48,7 @@ public abstract class Game {
     /**
      * Play the game. This might be one method or many method calls depending on your game.
      */
-    public abstract void play();
+    public abstract void play();//black jack, uno, war 
 
     /**
      * When the game is over, use this method to declare and display a winning player.
